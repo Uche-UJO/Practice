4e561735cc685df4579153fda741c3896af0c17f
Uche Obianyimuo <uutchei@gmail.com>
2020-11-15 21:49:50 -0600

    added java file

---
+++ practice for java.js
@@ -0,0 +1,44 @@
+// forEach
+//[1,2,3].forEach(function(item,index){
+//console.log(item, index);   
+//});
+
+//******************************//
+
+// map (you can use const instead of var)
+//var three = [1,2,3]
+//var doubled = three.map(function(item){
+//    return item *2;
+//});
+//console.log(doubled);
+
+//***********************************//
+
+// filter
+//const ints = [1,2,3];
+//const evens = ints.filter(function(item){
+//    return item % 2 === 0;
+//});
+//console.log(evens);
+
+//********************************//
+
+// reduce
+//const sum = [1,2,3].reduce(function(result, item){
+//    return result + item;
+//}, 0);
+//console.log(sum)
+
+//**************************************//
+
+
+// some
+//const hasNegativeNumbers = [1,2,3,-1,4].some(function(item){
+//    return item < 0;
+//});
+//console.log(hasNegativeNumbers);
+
+
+//*******************************************//
+
+
